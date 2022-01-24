706eab05647042437c33c9425c596da22531c9f9
Xiaodong Liu <xiaodl@microsoft.com>
2022-01-24 11:29:51 -0800

    cleaning code

--- pretrained_models.py
+++ pretrained_models.py
@@ -1,8 +1,16 @@
-from transformers import *
+from transformers import (
+    BertConfig, BertModel, BertTokenizer,
+    RobertaConfig, RobertaModel, RobertaTokenizer,
+    AlbertConfig, AlbertModel, AlbertTokenizer,
+    XLMRobertaConfig, XLMRobertaModel, XLMRobertaTokenizer,
+    ElectraConfig, ElectraModel, ElectraTokenizer,
+    T5Config, T5EncoderModel, T5ForConditionalGeneration, T5Tokenizer,
+    DebertaConfig, DebertaModel, DebertaTokenizer,
+)
+
 from module.san_model import SanModel
 MODEL_CLASSES = {
     "bert": (BertConfig, BertModel, BertTokenizer),
-    "xlnet": (XLNetConfig, XLNetModel, XLNetTokenizer),
     "roberta": (RobertaConfig, RobertaModel, RobertaTokenizer),
     "albert": (AlbertConfig, AlbertModel, AlbertTokenizer),
     "xlm": (XLMRobertaConfig, XLMRobertaModel, XLMRobertaTokenizer),
